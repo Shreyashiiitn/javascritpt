ca528b968ebdb855c40f795c65ac6293ad2bf4ac
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-22 09:05:05 +0000

    conversions and tricky questions

---
+++ basics/04_stringtonum.js
@@ -0,0 +1,46 @@
+console.log("string to numbers") 
+console.log("Numbers")
+
+let value = 3 ; 
+let negvalue = -value ; 
+console.log(negvalue) ; // output will be -3 
+
+// lets discuss operations 
+console.log(2 + 2) 
+console.log(2 - 2) 
+console.log(2 * 2) 
+console.log(2**2) // 2^2 i.e 4
+
+
+// Tricky things
+
+let str1 = "hitesh"
+let str2 = " hitesh" 
+let str3 = str1 + str2 ; 
+console.log(str3) ; 
+
+console.log( 1 + "2") ;  // output will be 12 
+console.log( "1" + 2 ) ;  // output will be 12 
+
+console.log(1 + 2 + "2") // this output will be 32 
+//  this is due to the guidelines of the ecmascript 
+
+
+console.log(+true) ; // this will give output as 1 
+console.log(+"") ;  // this will give as 0 
+
+
+let num1 , num2 , num3 ; 
+num1 = num2 = num3 = 2 + 2 ; // smart but not readable \
+// use the code redable 
+
+
+let gamecounter = 100 ; 
+gamecounter++ ; 
+console.log(gamecounter) ; // 101
+++gamecounter ; 
+console.log(gamecounter) ; // 102 
+
+// all the prefix and postfix will be done from the MDM and ES documentation 
+
+
