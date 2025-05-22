4f7cb5489fe42ec6aba7a6e98b6cd1d604297092
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-22 03:02:45 +0000

    datatypes

---
+++ basics/02_datatype.js
@@ -0,0 +1,16 @@
+console.log("datatypes");
+console.log(7 + 7) ; 
+
+ 
+let name  = "shreyash"
+let age = 18
+let isvalid = true 
+
+
+// number , string , bigint , 
+// boolean , true or false 
+// null 
+// symbol => unique 
+// objects , how to work on them 
+
+console.log(typeof("hitesh")) ; 
