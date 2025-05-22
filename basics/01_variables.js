dd0f3fed40254d7a49176231cea95932888b6f16
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-22 02:51:54 +0000

    about var , let etc

---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId  = 5287989524 ; 
+let accountemail = "shreyashraj@712004.gmailcom" ; 
+var accountpass = "shrey" ; 
+accountcity = "Jaipur" ;
+let accountstate ; // will be markwed as undefined 
+
+// accountId = 1 ; // not allowed , const cannnot be changed 
+
+accountemail = "raj.gmail.com" ; 
+accountpass = "hjafhjkahf" ; 
+accountcity = "nagpur" ;
+
+console.log(accountId);
+console.table([accountemail , accountpass , accountcity , accountstate]) ; 
