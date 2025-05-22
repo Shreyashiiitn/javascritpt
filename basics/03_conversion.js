3b7ac09677e534e4c1b6ac90c59680d7bbdddfee
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-22 08:51:47 +0000

    conversion study

---
+++ basics/03_conversion.js
@@ -0,0 +1,20 @@
+let score = "77" ;
+
+// while working on the backend 
+console.log(typeof(score)) ; 
+
+
+let valueinnumber = Number(score) ;
+console.log(typeof(score)) ;
+ 
+
+// "33" -> 33
+// "22abc" -> NAN , not a number
+// true -> 1 
+
+
+let isloggedin =  10; 
+let booleanisloggedin = Boolean(isloggedin) ; 
+console.log(booleanisloggedin) ; 
+
+// 1 -> true ; 0 -> false 
