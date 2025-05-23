c7ea6ce2a256deb1183500fb89c742250831004e
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-23 10:18:40 +0000

    comparison

---
+++ basics/05_comparison.js
@@ -0,0 +1,12 @@
+console.log(2 < 5) ; 
+console.log(2 == 2) ; 
+
+// these all are simple comparisons 
+// problems arises when we dont compare same datatypes 
+
+console.log("2" > 1) ; 
+// typescript only provides some rules to do this and not to do this 
+
+console.log(null > 0) ; 
+console.log(null ==  0) ; 
+console.log(null >= 0) ; 
