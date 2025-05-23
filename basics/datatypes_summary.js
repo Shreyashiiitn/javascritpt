57b2e7c8f3ee7ada28dac504289b7ff7f77534b4
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-23 10:29:52 +0000

    Interview type question

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+const score = 100 ; 
+const id = Symbol('123') ; 
+const anotherid = Symbol('123') ; 
+
+console.log(id === anotherid); //  will give false 
+console.log(anotherid) ; 
+
+const bigNumber = 78148971439878349n ; // this is the bigint 
+
+
+
+// Non primitive datatypes 
+const heroes = ["shaktiman" , "naarang" , "doga"] ; 
+let myobje = {
+    name : "shreyash" , 
+    age : 24 , 
+}
+
+const myfucntion = function(){
+    console.log("Hellow world");
+}
+
