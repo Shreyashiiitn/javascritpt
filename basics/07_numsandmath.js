8ab669e2b92f92d51d9e0d3ddfe348103b669d3b
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-23 11:16:50 +0000

    math and num

---
+++ basics/07_numsandmath.js
@@ -0,0 +1,39 @@
+// Alll about numbers 
+const score = 100 ; 
+console.log(score);
+
+const balance = new Number(10000) ; 
+console.log(balance);
+
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2)); // will show values till 2 after the decimal value 
+
+
+//++++++++++++++++ Maths 
+console.log(Math) ; // return a objectt 
+
+console.log(Math.abs(-7));
+console.log(Math.round(7.7));
+
+
+console.log(Math.ceil(4.0001));
+console.log(Math.floor(3.99999));
+
+
+console.log(Math.random()); // between 0 and 1 
+console.log((Math.random()*10) + 1);
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min)) ; 
+
+
+
+
+
+
+
