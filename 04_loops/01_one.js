3ea6679e1a08beaae2e164db024f5dba8d99bae0
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-26 06:17:00 +0000

    for loops

---
+++ 04_loops/01_one.js
@@ -0,0 +1,16 @@
+// for looops 
+let myarray = ["shreyash" , "is" , "a" , "good"] ; 
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element);
+    
+}
+
+
+// conditions on loop 
+// break and continue 
+for(let i = 0 ; i<myarray.length ; i++){
+    if(i === 2) break ; 
+    console.log(myarray[i]);
+    
+}
