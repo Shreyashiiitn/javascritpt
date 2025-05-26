2e01321b7228f478cc3b0c57854a9ce19feec9b5
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-26 06:22:04 +0000

    while and do while loop

---
+++ 04_loops/02_two.js
@@ -0,0 +1,15 @@
+// while loop
+let ind = 0 ; 
+while (ind <= 10) {
+    console.log(ind++);
+}
+
+console.log("while loop end , do while started");
+
+
+do{
+    console.log(ind++) ; 
+}
+while(ind <= 20)
+
+
