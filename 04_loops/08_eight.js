fc5885931321c80f6da11bb10def23383e4785e6
Shreyashiiitn <118331370+Shreyashiiitn@users.noreply.github.com>
2025-05-26 10:46:01 +0000

    all about maps in js

---
+++ 04_loops/08_eight.js
@@ -0,0 +1,19 @@
+// maps 
+const mynums = [1 , 2 , 3 , 4, 5 ,6, 7 ,8,  9, 10] ;
+// to add 10 to each of them 
+// map is a callback function 
+
+const newnums = mynums.map((num) => {
+    return num + 10 ; 
+})
+
+console.log(newnums);
+
+
+// chaining 
+const newnums1 = mynums
+.map((num) => num*10)
+.map((num) => num + 1)
+.filter((num) => num > 40)
+
+console.log(newnums1);
